e009270b58a049c027c144a195d7485504bb2ba0
Shall16 <shall130@rsdmo.org>
2018-06-23 00:58:31 +0000

    automatically backed up by learn

---
+++ argument.rb
@@ -0,0 +1,11 @@
+def greeting (local_greeting="hello",your_name, my_name, time_of_day)
+  puts "#{local_greeting},#{your_name}, I am #{my_name} and the time is #{time_of_day}"
+end
+
+greeting ("Nate","Sam","7:00pm")
+
+#def farewell
+ # puts "#{local_goodbyes,#{your_name}, use #{code} for 10 percent off, and it is now #{time}
+#end
+
+#farewell("See you next time","")
---
+++ morning_routine.rb
@@ -0,0 +1,7 @@
+def morning_routine
+  puts "Shelly gets out of bed and turns the light on"
+  puts "I go back to sleep"
+  puts "I get out of bed"
+  puts "change to proper close "
+end 
+morning_routine
